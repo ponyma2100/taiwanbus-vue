3228fe18b9496cb58d9aa9f4d89e389057927436
Pony <ponyma2100@gmail.com>
2021-12-16 20:06:09 +0800

    Add css style in CityMenu.vue

--- src/components/CityMenu.vue
+++ src/components/CityMenu.vue
@@ -2,7 +2,7 @@
   <div class="citymenu">
     <div class="citybtn" v-for="city in cityName" :key="city.CityID">
       <router-link :to="{ name: 'CityBus', params: { city: city.City } }">
-        <button class="btn">
+        <button class="btn" :class="city.City">
           {{ city.CityName }}
           <p>{{ city.City }}</p>
         </button>
@@ -32,5 +32,21 @@ export default {
   height: 18vh;
   width: 217px;
   margin: 20px 20px;
+}
+.btn.NewTaipei {
+  color: #52797c;
+}
+
+.btn.Taoyuan {
+  color: #3591c5;
+}
+.btn.Taoyuan {
+  color: #3591c5;
+}
+.btn.Taichung {
+  color: #d08181;
+}
+.btn.Tainan {
+  color: #7fc0c5;
 }
 </style>
